b5125b98da11cc116869f6ae9c20ff1fee1e060d
Eric <54079742+Eric-programming@users.noreply.github.com>
2022-07-03 18:49:24 -0700

    Update Index.ts

--- Index.ts
+++ Index.ts
@@ -1,5 +1,14 @@
-interface DataPoint {
-  time: number; // in seconds
-  sentence: string;
-}
-function convertVideoFile(mp4: any): DataPoint[] {}
+const clips = [
+  {
+    url: "https://youtu.be/hIHW5AI35SQ",
+    start: "1:01",
+    end: "2:01",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=sUwD3GRPJos",
+    start: "1:01",
+    end: "10:01",
+  }
+]
+
+downloadYTClips(clips, "/videos")
